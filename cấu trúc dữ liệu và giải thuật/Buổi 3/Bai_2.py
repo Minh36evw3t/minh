00e25d95d105974420e6756c8f1cc75185753022
Minh36evw3t <157337247+Minh36evw3t@users.noreply.github.com>
2024-01-29 14:10:39 +0700

    Add files via upload

---
+++ cấu trúc dữ liệu và giải thuật/Buổi 3/Bai_2.py
@@ -0,0 +1,6 @@
+myList = [1,2,3,4,5,6,7,8,9]# myList là một danh sách (list) chứa các số từ 1 đến 9.
+print(myList)#in ra danh sách myList ban đầu, và kết quả sẽ là [1, 2, 3, 4, 5, 6, 7,8,9]
+myList[1] = 25#gán giá trị 25 cho phần tử thứ 2 của danh sách (myList), vì chỉ số bắt đầu từ 0, nên đây là phần tử thứ 2.
+myList[3] = 34#gán giá trị 34 cho phần tử thứ 4 của danh sách (myList).
+
+print(myList)#in ra danh sách myList sau khi đã thay đổi, và kết quả sẽ là [1, 25, 3, 34, 5, 6, 7,8,9].
